ffd9f75d2be9f41bd78304edfd97ef1ed25cdf26
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2025-02-13 12:15:17 +0530

    cloudwatch log kms variable refactor

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_log_group" "this" {
   count             = module.context.enabled ? 1 : 0
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = var.cloudwatch_logs_retention_in_days
-  kms_key_id        = var.cloudwatch_kms_key_id
+  kms_key_id        = var.cloudwatch_logs_kms_key_arn
   tags              = module.context.tags
 }
 
